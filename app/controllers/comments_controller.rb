37bae39196e7c5443754524ea984e526d225a56c
madeindjs <madeindjs@gmail.com>
2016-03-08 22:43:29 +0100

    Add feature to add a comment from recipe sheet.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -76,6 +76,6 @@ class CommentsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def comment_params
-      params.require(:comment).permit(:title, :content, :user_id)
+      params.require(:comment).permit(:title, :content, :user_id , :recipe_id)
     end
 end
