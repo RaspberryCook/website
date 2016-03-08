b9b7af8115e5bd91994eac3c8ab2de5441338c50
madeindjs <madeindjs@gmail.com>
2016-03-08 19:51:01 +0100

    some setup about comments

--- Gemfile
+++ Gemfile
@@ -44,6 +44,8 @@ group :development do
     gem 'capistrano-rvm',     require: false
     gem 'capistrano-rails',   require: false
     gem 'capistrano-bundler', require: false
+
+    # gem 'ruby-debug'
 end
 
 gem 'puma'
@@ -51,7 +53,7 @@ gem 'puma'
 
 
 #for upload picture
-gem 'carrierwave', :github => 'satoruk/carrierwave' , :ref => '43179f94d6a4e62f69e812f5082d6447c9138480'
+gem 'carrierwave'
 gem 'rmagick', require: false
 # gem 'rmagick' # sudo apt-get install libmagickwand-dev
 # gem 'mini_magick'
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -26,17 +26,22 @@ class CommentsController < ApplicationController
   # POST /comments
   # POST /comments.json
   def create
-    @comment = Comment.new(comment_params)
 
-    respond_to do |format|
-      if @comment.save
-        format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @comment }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
-      end
-    end
+    @comment = Comment.new(comment_params)
+    @comment.user_id = @current_user
+
+    render :text => @current_user.inspect
+
+
+    # respond_to do |format|
+    #   if @comment.save
+    #     format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
+    #     format.json { render action: 'show', status: :created, location: @comment }
+    #   else
+    #     format.html { render action: 'new' }
+    #     format.json { render json: @comment.errors, status: :unprocessable_entity }
+    #   end
+    # end
   end
 
   # PATCH/PUT /comments/1
