2e5dcb5554ee7217e790187af95c6e503c7e6b47
Rousseau Alexandre <RousseauAlexandre.Lyon@gmai.com>
2016-03-09 23:05:26 +0100

    begin to setup a way to hide form if you have already made a comment on this recipe

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,16 @@ class Comment < ActiveRecord::Base
  	belongs_to :user #association avec la table des utilisateurs
  	belongs_to :recipe #association avec la table des utilisateurs
 
+ 	public 
+
+ 	def self.user_exist? user_id
+ 		result = self.where 'user_id' => user_id
+
+ 		if result = nil
+ 			false
+ 		else
+ 			true
+ 		end
+ 	end
 
 end
