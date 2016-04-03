eea8ab958102feb8be9b41af89e51d5c10aeeef6
Rousseau Alexandre <RousseauAlexandre.Lyon@gmai.com>
2016-04-03 16:51:04 +0200

    generate a new secret token and add it to gitignore file
    :q!

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,7 +25,7 @@ set :repo_url, 'https://github.com/madeindjs/raspberry_cook.git'
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml')
 
 # Default value for linked_dirs is []
 # set :linked_dirs, fetch(:linked_dirs, []).push('log', 'public/uploads')
@@ -39,7 +39,7 @@ set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/
 namespace :deploy do
 
   after :updated, :bundle_install do
-  	invoke "ruby_on_rails:copy_database_configuration"
+  	# invoke "ruby_on_rails:copy_database_configuration"
   	# invoke "ruby_on_rails:bundle_install"
   	# invoke "ruby_on_rails:db_migrate"
   	# invoke "ruby_on_rails:assets_precompile"
