7fa9301fe160c75538201a11b12880b17b9a2aec
madeindjs <madeindjs@gmail.com>
2016-07-05 19:01:34 +0200

    set title for  404 & 500

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,8 +1,11 @@
 class ErrorsController < ApplicationController
   def not_found
   	render(:status => 404)
+	@title = '404'
   end
 
   def internal_server_error
+  	render(:status => 500)
+	@title = '500'
   end
 end
