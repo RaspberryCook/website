e7acace266057dd70c9d3d130aaf1b18531ef954
madeindjs <madeindjs@gmail.com>
2016-09-11 16:26:52 +0200

    delete unused files and edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,60 +1,32 @@
 RaspberryCook::Application.routes.draw do
+
+  resources :users
+  match '/signup' , :to => 'users#new', :via => [:get, :post]
+
+  resources :recipes
+  get "recipes/save"
+  get "recipes/search"
+  get 'recipes/fork'
+  match 'recipes/save/:id' ,   :to => 'recipes#save', :via => [:get, :post]
+  match 'recipes/fork/:id' ,   :to => 'recipes#fork', :via => :get
+  match 'recipes/fork' ,   :to => 'recipes#fork', :via => :post
+  match 'recipes/vote' , :to => 'recipes#vote', :via => :get
+
+  resources :sessions, :only => [:new , :create , :destroy ]
+  match '/signin' , :to => 'sessions#new', :via => [:get, :post]
+  match '/signout' ,:to => 'sessions#destroy', :via => [:get, :post]
   
   get "errors/not_found"
   get "errors/internal_server_error"
-
   match "/404", :to => "errors#not_found", :via => :all
   match "/500", :to => "errors#internal_server_error", :via => :all
 
-  
-
-  resources :comments
-
   get "pages/credits"
-
   get "pages/home"
   get "pages/infos"
-
-  post "ingredients/search"
-
-  get "recipes/index"
-  get "recipes/save"
-  get "recipes/search"
-  get 'recipes/fork' 
-  match 'recipes/fork/:id' ,   :to => 'recipes#fork', :via => :get
-  match 'recipes/fork' ,   :to => 'recipes#fork', :via => :post
-  match 'recipes/vote' , :to => 'recipes#vote', :via => [:get]
-
-
-  get "sessions/new"
-
-  get "users/new"
-  get "users/index"
-  get "users/edit"
-
-  match '/signup' , :to => 'users#new', :via => [:get, :post]
-
   match '/home' ,   :to => 'pages#home', :via => [:get, :post]
   match '/infos' ,  :to => 'pages#infos', :via => [:get, :post]
   match '/credits' ,  :to => 'pages#credits', :via => :get
-
-  match 'recipes/save/:id' ,   :to => 'recipes#save', :via => [:get, :post]
-
-	
-	match '/signin' , :to => 'sessions#new', :via => [:get, :post]
-	match '/signout' ,:to => 'sessions#destroy', :via => [:get, :post]
-
-
-  resources :users
-
-  resources :recipes do 
-    get :autocomplete_ingredient_name, :on => :collection
-  end
-
-  resources :ingredients, :only => [:index , :search ]
-  resources :sessions, :only => [:new , :create , :destroy ]
-
-
 
   root :to => 'pages#home'
 
