2d16189fd7e1781529fecca6d7a36994c7ef0964
madeindjs <madeindjs@gmail.com>
2016-12-01 16:38:38 +0100

    add documentation on helpers

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,17 +1,23 @@
+# Helper user in all views to check if someone is logged
 module SessionsHelper
 
-
+	# Check if someone is logged in
+	#
+	# @return [Boolean] as true if someone is logged
 	def signed_in?
 		return true if  current_user else false
 	end
 
-	#check if the curentuser correspond to one user
-	def current_user?(user)
+	# Check if the curentuser correspond to one user
+	#
+	# @param user [User] as user to compare at current user
+	# @return [Boolean] as true if given user correspond to current user
+	def current_user? user
 		user == current_user
 	end
 
 
-	#ONLY FOR THE REDIRECTION IN CASE OF TRY TO ACCESS ON BAD SHEETS
+	# Only for the redirection in case of try to access on bad sheets
 	def deny_access
 		store_location
 		redirect_to signin_path, :notice => "Please login to access on this sheet."
