24876a7ea351f023d55897d4ccd4802ca36d704c
madeindjs <madeindjs@gmail.com>
2016-12-21 13:12:13 +0100

    setup flash messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -67,7 +67,7 @@ class UsersController < ApplicationController
 			flash[:success] = "Profil mis a jour"
 			redirect_to @user
 		else
-			flash[:error] = "Une erreur est survenue"
+			flash[:warning] = "Une erreur est survenue"
 			redirect_to edit_user_path(@user)
 		end
 	end
@@ -86,7 +86,7 @@ class UsersController < ApplicationController
 
 		def correct_user
 			@user = User.find(params[:id])
-			redirect_to root_path , :notice => "petit coquin!" unless current_user == @user
+			redirect_to root_path , :info => "petit coquin!" unless current_user == @user
 		end
 
 		def admin_user
