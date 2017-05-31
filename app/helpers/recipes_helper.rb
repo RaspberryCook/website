c06a3d07a2ec631858e165527e26d83e2e80e577
madeindjs <madeindjs@gmail.com>
2017-05-31 13:55:56 +0200

    Make a clearer recipe design

--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -9,8 +9,8 @@ module RecipesHelper
 	# @return [nil] if it's not possible to create Html content
 	def recipe_label_type recipe
 		if recipe.rtype
-			image_tag = image_tag "type_of_food/%s.svg" % recipe.rtype, class: "recipe-label"
-			link_to image_tag + recipe.rtype , recipes_path( :type => recipe.rtype )  
+			html_image = image_tag "type_of_food/%s.svg" % recipe.rtype, class: "recipe-label", alt: 'Type de plat', size: '40'
+			link_to html_image + recipe.rtype , recipes_path( :type => recipe.rtype )
 		end
 	end
 
@@ -22,8 +22,8 @@ module RecipesHelper
 	# @return [nil] if it's not possible to create Html content
 	def recipe_label_season recipe
 		if recipe.rtype
-			image_tag = image_tag "seasons/%s.svg" % recipe.season, class: "recipe-label"
-			link_to  image_tag + recipe.season , recipes_path( :season => recipe.season ) 
+			html_image = image_tag "seasons/%s.svg" % recipe.season, class: "recipe-label", alt: 'Icon de la saison', size: '40'
+			link_to  html_image + recipe.season , recipes_path( :season => recipe.season )
 		else
 			nil
 		end
@@ -38,11 +38,11 @@ module RecipesHelper
 	# @return [nil] if it's not possible to create Html content
 	def recipe_label_time recipe, time_name
 		time_of_recipe = recipe.public_send time_name
-					
+
 		if time_of_recipe
 			time = "<time>%s\"</time>" % time_of_recipe
-			image_tag = image_tag "time/#{time_name}.svg", class: "recipe-label"
-			"#{image_tag} #{time}".html_safe
+			html_image = image_tag "time/#{time_name}.svg", class: "recipe-label"
+			"#{html_image} #{time}".html_safe
 		else
 			nil
 		end
