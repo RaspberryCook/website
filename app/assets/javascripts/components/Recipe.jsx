ee86f7f0c4390ea606d32c00d3587edf50bdd934
Alexandre Rousseau <madeindjs@gmail.com>
2017-06-18 17:00:46 +0200

    Finish to move recipe into a React component

--- app/assets/javascripts/components/Recipe.jsx
+++ app/assets/javascripts/components/Recipe.jsx
@@ -1,9 +1,14 @@
 class Recipe extends React.Component {
   render() {
-    return <figure className="recipe" >
-
-			<a>{this.props.name}</a>
-
-		</figure>
+    return <div className="col-md-3 col-sm-4 col-xs-6">
+      <figure className="recipe" >
+        <a href={this.props.url}>
+          <img src={this.props.imageUrl} className='img-responsive img-rounded' alt="Image for {this.props.name}" />
+        </a>
+        <figcaption>
+          <a href={this.props.url}>{this.props.name}</a>
+        </figcaption>
+  		</figure>
+    </div>
   }
 }
