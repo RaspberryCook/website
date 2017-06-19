2577ca70568ac2654af9b63868a8b1a266abc171
madeindjs <madeindjs@gmail.com>
2017-06-19 13:57:32 +0200

    Display ingredients when recipe is hovered

--- app/assets/javascripts/components/Recipe.jsx
+++ app/assets/javascripts/components/Recipe.jsx
@@ -2,32 +2,52 @@ class Recipe extends React.Component {
 
   constructor(props){
     super(props);
-    this.state = {name: this.props.name};
+    this.state = {isHovered: false};
     // declare event here
     this.handleHover = this.handleHover.bind(this);
   }
 
   handleHover(){
     this.setState({
-      name: 'Hello'
+      isHovered: !this.state.isHovered
     });
   }
 
   render() {
-    return (
-      <div
+    if(this.state.isHovered){
+      return (
+        <div
         className="col-md-3 col-sm-4 col-xs-6"
         onMouseEnter={this.handleHover}
-      >
-        <figure className="recipe" >
-          <a href={this.props.url}>
-            <img src={this.props.imageUrl} className='img-responsive img-rounded' alt="Image for {this.props.name}" />
-          </a>
-          <figcaption>
-            <a href={this.props.url}>{this.state.name}</a>
-          </figcaption>
-    		</figure>
-      </div>
-    )
+        onMouseLeave={this.handleHover}
+        >
+          <figure className="recipe" >
+            <ul>
+              <li>{this.props.ingredients}</li>
+            </ul>
+            <figcaption>
+              <a href={this.props.url}>{this.props.name}</a>
+            </figcaption>
+          </figure>
+        </div>
+      )
+
+    }else{
+      return (
+        <div
+        className="col-md-3 col-sm-4 col-xs-6"
+        onMouseEnter={(this.handleHover)}
+        >
+          <figure className="recipe" >
+            <a href={this.props.url}>
+              <img src={this.props.imageUrl} className='img-responsive img-rounded' alt="Image for {this.props.name}" />
+            </a>
+            <figcaption>
+              <a href={this.props.url}>{this.props.name}</a>
+            </figcaption>
+          </figure>
+        </div>
+      )
+    }
   }
 }
