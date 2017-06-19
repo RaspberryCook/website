36db3a6c9c4afa7a3e3d62f36c1d79db0d4486b3
madeindjs <madeindjs@gmail.com>
2017-06-19 12:54:46 +0200

    Handling events

--- app/assets/javascripts/components/Recipe.jsx
+++ app/assets/javascripts/components/Recipe.jsx
@@ -1,14 +1,33 @@
 class Recipe extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {name: this.props.name};
+    // declare event here
+    this.handleHover = this.handleHover.bind(this);
+  }
+
+  handleHover(){
+    this.setState({
+      name: 'Hello'
+    });
+  }
+
   render() {
-    return <div className="col-md-3 col-sm-4 col-xs-6">
-      <figure className="recipe" >
-        <a href={this.props.url}>
-          <img src={this.props.imageUrl} className='img-responsive img-rounded' alt="Image for {this.props.name}" />
-        </a>
-        <figcaption>
-          <a href={this.props.url}>{this.props.name}</a>
-        </figcaption>
-  		</figure>
-    </div>
+    return (
+      <div
+        className="col-md-3 col-sm-4 col-xs-6"
+        onMouseEnter={this.handleHover}
+      >
+        <figure className="recipe" >
+          <a href={this.props.url}>
+            <img src={this.props.imageUrl} className='img-responsive img-rounded' alt="Image for {this.props.name}" />
+          </a>
+          <figcaption>
+            <a href={this.props.url}>{this.state.name}</a>
+          </figcaption>
+    		</figure>
+      </div>
+    )
   }
 }
