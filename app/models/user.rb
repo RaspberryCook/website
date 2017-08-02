e2035992c5083de3827eebf89b5fb0c1a384cb15
madeindjs <madeindjs@gmail.com>
2017-08-02 12:50:57 +0200

    Format

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,75 +13,72 @@
 class User < ActiveRecord::Base
 
 
-	attr_accessible :username, :firstname, :lastname, :email,
-		:password, :password_confirmation, :crypted_password
-	
+  attr_accessible :username, :firstname, :lastname, :email,
+    :password, :password_confirmation, :crypted_password
+  
 
-	has_many :recipes , :dependent => :destroy
-	has_many :comments , :dependent => :destroy
+  has_many :recipes , :dependent => :destroy
+  has_many :comments , :dependent => :destroy
 
-	email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
-	validates :email, :presence => true , :format => { :with => email_regex }, :uniqueness => { :case_sensitive => false }
-	validates :username , :presence  => true , :uniqueness => true
-	validates :firstname , :presence => true , :length  => { :maximum => 50 }
-	validates :password, :presence => true, :confirmation => true, :length => { :within => 6..40 }
+  validates :email, :presence => true , :format => { :with => email_regex }, :uniqueness => { :case_sensitive => false }
+  validates :username , :presence  => true , :uniqueness => true
+  validates :firstname , :presence => true , :length  => { :maximum => 50 }
+  validates :password, :presence => true, :confirmation => true, :length => { :within => 6..40 }
 
-	acts_as_authentic do |c|
-		c.crypto_provider = Authlogic::CryptoProviders::Sha512
-		c.validate_email_field = true
-	end
+  acts_as_authentic do |c|
+    c.crypto_provider = Authlogic::CryptoProviders::Sha512
+    c.validate_email_field = true
+  end
 
-	acts_as_reader
+  acts_as_reader
 
-	public
-
-
-		# Construct the complete name as "John Doe"
-		#
-		# @return [String] as complete name
-		def complete_name
-			return '%s %s' % [ self.lastname, self.firstname ]
-		end
+  # Construct the complete name as "John Doe"
+  #
+  # @return [String] as complete name
+  def complete_name
+    return '%s %s' % [ self.lastname, self.firstname ]
+  end
 
 
-		# Get user's comment on the specified recipe id
-		#
-		# @param recipe_id [Integer] as Id of the recipe
-		# @return [Comment] as comment founded
-		def comment_on_recipe recipe_id
-			comment = Comment.where( :user_id => self.id , :recipe_id => recipe_id ).first
-			comment = Comment.new if comment.blank?
-			return comment
-		end
+  # Get user's comment on the specified recipe id
+  #
+  # @param recipe_id [Integer] as Id of the recipe
+  # @return [Comment] as comment founded
+  def comment_on_recipe recipe_id
+    comment = Comment.where( :user_id => self.id , :recipe_id => recipe_id ).first
+    comment = Comment.new if comment.blank?
+    return comment
+  end
 
 
-		# Check password equality with encrypt
-		#
-		# @param password_sent [String] as pasword sent by user
-		# @return [Boolean] if password correspond
-		def has_password? password_sent
-			return self.password == encrypt(password_sent)
-		end
+  # Check password equality with encrypt
+  #
+  # @param password_sent [String] as pasword sent by user
+  # @return [Boolean] if password correspond
+  def has_password? password_sent
+    return self.password == encrypt(password_sent)
+  end
 
 
-		# Connect user to Rasberry-cook.fr
-		#
-		# @param email [String] as email sent by user
-		# @param submitted_password [String] as pasword sent by user
-		# @return [Boolean] as true if success
-		def self.authenticate email, submitted_password
-			user = find_by_email(email)
-			return nil if user.nil?
-			return user if user.has_password?(submitted_password)
-		end
+  # Connect user to Rasberry-cook.fr
+  #
+  # @param email [String] as email sent by user
+  # @param submitted_password [String] as pasword sent by user
+  # @return [Boolean] as true if success
+  def self.authenticate email, submitted_password
+    user = find_by_email(email)
+    return nil if user.nil?
+    return user if user.has_password?(submitted_password)
+  end
 
 
-		# Get rank of this user nRecipes*10 + nComments
-		#
-		# @return [Integer] as score
-		def rank
-			self.comments.count + self.recipes.count*5
-		end
+  # Get rank of this user nRecipes*10 + nComments
+  #
+  # @return [Integer] as score
+  def rank
+    self.comments.count + self.recipes.count*5
+  end
 
 end
