e3d660ca2320e93b15644dceed7775b6c592440b
madeindjs <madeindjs@gmail.com>
2017-10-10 13:39:09 +0200

    Correct repository URL

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ lock '3.7.2'
 set :default_env, { rvm_bin_path: '~/.rvm/bin' }
 
 set :application, 'raspberry_cook'
-set :repo_url, 'https://github.com/raspberry_cook/website.git'
+set :repo_url, 'https://github.com/RaspberryCook/website.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
