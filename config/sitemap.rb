6300cbecde83709737fcca0307f13b020bdf453b
madeindjs <madeindjs@gmail.com>
2017-11-02 12:31:42 +0100

    Pass website to https

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,5 +1,5 @@
 # Set the host name for URL creation
-SitemapGenerator::Sitemap.default_host = 'http://raspberry-cook.fr'
+SitemapGenerator::Sitemap.default_host = 'https://raspberry-cook.fr'
 
 SitemapGenerator::Sitemap.create do
   add home_path
