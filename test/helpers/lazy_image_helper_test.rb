27fdaa5d7957ef2306f4a3dfdd9f849e779e3a33
madeindjs <madeindjs@gmail.com>
2017-11-10 12:42:27 +0100

    Correct order of html attributes for unit tests

--- test/helpers/lazy_image_helper_test.rb
+++ test/helpers/lazy_image_helper_test.rb
@@ -7,11 +7,11 @@ class RecipeHelperTest < ActionView::TestCase
 
   test "should create image tag with lazy loader" do
     # simple use
-    assert_equal '<img data-src="/images/github.svg" class="lazyload" src="/images/default.svg" alt="github" />', lazy_image_tag('github.svg')
+    assert_equal '<img data-src="http://test.host/images/github.svg" class="lazyload" src="http://test.host/images/default.svg" alt="Default" />', lazy_image_tag('github.svg')
 
-    assert_equal '<img data-src="/images/github.svg" class="test lazyload" src="/images/default.svg" alt="github" />', lazy_image_tag('github.svg', class: 'test')
+    assert_equal '<img class="test lazyload" data-src="http://test.host/images/github.svg" src="http://test.host/images/default.svg" alt="Default" />', lazy_image_tag('github.svg', class: 'test')
 
-    assert_equal '<img  data-src="/images/github.svg" class="test lazyload" src="/images/default.svg" alt="hello" />', lazy_image_tag('github.svg', class: 'test', alt: 'hello')
+    assert_equal '<img class="test lazyload" alt="hello" data-src="http://test.host/images/github.svg" src="http://test.host/images/default.svg" />', lazy_image_tag('github.svg', class: 'test', alt: 'hello')
   end
 
 
