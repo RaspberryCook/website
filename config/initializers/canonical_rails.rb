0481fb1a0ba62de5180990f695a34ab5d5a904c7
Rousseau Alexandre <contact@rousseau-alexandre.fr>
2018-08-03 12:32:34 +0200

    Remove HTTPS

--- config/initializers/canonical_rails.rb
+++ config/initializers/canonical_rails.rb
@@ -4,7 +4,7 @@ CanonicalRails.setup do |config|
 
   # Force the protocol. If you do not specify, the protocol will be based on the incoming request's protocol.
 
-  config.protocol= 'https://'
+  config.protocol= 'http://'
 
   # This is the main host, not just the TLD, omit slashes and protocol. If you have more than one, pick the one you want to rank in search results.
 
